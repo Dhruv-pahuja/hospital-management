cd64b9305505e4460ce13ceb1915ec47689ed3f5
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2025-02-24 22:10:16 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/opdQueue', require('./routes/opdQueue'));
